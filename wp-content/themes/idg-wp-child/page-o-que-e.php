885a140709cc03d198d9586db62a92c221199773
Daniel Moura <daniel.mouraa@gmail.com>
2019-03-08 18:51:36 -0300

    [UPDATE] Ajuste tela o que e

--- wp-content/themes/idg-wp-child/page-o-que-e.php
+++ wp-content/themes/idg-wp-child/page-o-que-e.php
@@ -17,10 +17,10 @@
 
     <div class="container image-right no-border">
       <div class="col-image text-center">
-        <div class="box">
-          <div class="img-wrapper mb-3">
+        <div class="box" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/03.jpg');">
+<!--           <div class="img-wrapper mb-3">
             <img src="http://portal-rouanet.infra.cultura.gov.br/wp-content/uploads/2019/03/03-150x150.jpg" width="400" />
-          </div>
+          </div> -->
         </div>
       </div>
 
@@ -33,91 +33,96 @@
         <ul class="horizontal-links">
           <li><a href="#incentivo">Incentivo</a></li>
           <li><a href="#fnc">FNC</a></li>
-          <li><a href="#ficart">FICART</a></li>
+          <li><a href="#ficart">Ficart</a></li>
         </ul>
       </div>
     </div>
   </section>
 
-  <section class="image-and-text bg-grey-2" id="incentivo">
-    <div class="container image-left">
-      <div class="col-image pt-0 pb-0">
-        <div class="box">
-          <h3>Incentivo</h3>
-          <p>O Incentivo a projetos culturais é um mecanismo em que a União faculta às pessoas, físicas ou jurídicas, a opção pela aplicação de parcelas do Imposto sobre a Renda em projetos vinculados à Cultura. Ou seja, trata-se de uma ferramenta, oferecida pelo governo federal, para que a sociedade possa decidir aplicar parte do dinheiro de seus impostos em ações culturais. Desta maneira, o incentivo fiscal estimula a participação dos cidadãos, da iniciativa privada e do mercado empresarial no aporte de recursos para o campo da Cultura, diversificando possibilidades de financiamento, ampliando o volume de recursos destinados ao setor, atribuindo a ele mais potência e mais estratégia econômica. O incentivo ainda cumpre interesse público ao promover maior democratização do acesso aos produtos culturais gerados pelos projetos, no âmbito da efetivação dos direitos culturais.</p>
+  <div class="boxes">
+
+    <section class="image-and-text bg-grey-2" id="incentivo">
+      <div class="container image-left">
+        <div class="col-image pt-0 pb-0">
+          <div class="box" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/01.jpg');">
+    <!--         <div class="img-wrapper mb-3">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/03.jpg" />
+            </div> -->
+            <h3>Incentivo</h3>
+            <p>O Incentivo a projetos culturais é um mecanismo em que a União faculta às pessoas, físicas ou jurídicas, a opção pela aplicação de parcelas do Imposto sobre a Renda em projetos vinculados à Cultura. Ou seja, trata-se de uma ferramenta, oferecida pelo governo federal, para que a sociedade possa decidir aplicar parte do dinheiro de seus impostos em ações culturais. Desta maneira, o incentivo fiscal estimula a participação dos cidadãos, da iniciativa privada e do mercado empresarial no aporte de recursos para o campo da Cultura, diversificando possibilidades de financiamento, ampliando o volume de recursos destinados ao setor, atribuindo a ele mais potência e mais estratégia econômica. O incentivo ainda cumpre interesse público ao promover maior democratização do acesso aos produtos culturais gerados pelos projetos, no âmbito da efetivação dos direitos culturais.</p>
+          </div>
+        </div>
+
+        <div class="col-text">
+          <h5>Está na lei:</h5>
+
+          <p><a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Do Incentivo a Projetos Culturais</a></p>
+          <p>Art. 18.  Com o objetivo de incentivar as atividades culturais, a União facultará às pessoas físicas ou jurídicas a opção pela aplicação de parcelas do Imposto sobre a Renda, a título de doações ou patrocínios, tanto no apoio direto a projetos culturais apresentados por pessoas físicas ou por pessoas jurídicas de natureza cultural, como através de contribuições ao FNC, nos termos do art. 5o, inciso II, desta Lei, desde que os projetos atendam aos critérios estabelecidos no art. 1o desta Lei.</p>
+          <p>§ 1o  Os contribuintes poderão deduzir do imposto de renda devido as quantias efetivamente despendidas nos projetos elencados no § 3o, previamente aprovados pelo Ministério da Cultura, nos limites e nas condições estabelecidos na legislação do imposto de renda vigente, na forma de: </p>
+          <p>a) doações; e</p>
+          <p>b) patrocínios.</p>
+          <p>§ 2o  As pessoas jurídicas tributadas com base no lucro real não poderão deduzir o valor da doação ou do patrocínio referido no parágrafo anterior como despesa operacional.</p>
+          <p>§ 3o  As doações e os patrocínios na produção cultural, a que se refere o § 1o, atenderão exclusivamente aos seguintes segmentos:</p>
+          <p>a) artes cênicas;</p>
+          <p>b) livros de valor artístico, literário ou humanístico;</p>
+          <p>c) música erudita ou instrumental; </p>
+          <p>d) exposições de artes visuais;</p>
+          <p>e) doações de acervos para bibliotecas públicas, museus, arquivos públicos e cinematecas, bem como treinamento de pessoal e aquisição de equipamentos para a manutenção desses acervos;</p>
+          <p>f) produção de obras cinematográficas e videofonográficas de curta e média metragem e preservação e difusão do acervo audiovisual; </p>
+          <p>g) preservação do patrimônio cultural material e imaterial.</p>
+          <p>h) construção e manutenção de salas de cinema e teatro, que poderão funcionar também como centros culturais comunitários, em Municípios com menos de 100.000 (cem mil) habitantes.</p>
         </div>
       </div>
+    </section>
 
-      <div class="col-text">
-        <h3>Está na lei:</h3>
+    <section class="image-and-text" id="fnc">
+      <div class="container image-left">
+        <div class="col-image pt-0 pb-0">
+          <div class="box" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/04.jpg');">
+            <h3>FNC</h3>
+            <p>O Fundo Nacional da Cultura representa o investimento direto do Estado no fomento à Cultura, já que o apoio a projetos decorre de recursos do orçamento da União. O apoio via FNC promove, por exemplo, a distribuição regional dos recursos de forma equilibrada. O fomento ocorre, principalmente, por meio de editais de seleção – o que proporciona aderência às políticas da Secretaria Especial da Cultura do Ministério da Cidadania, promovendo um equilíbrio regional, social e econômico.</p>
+          </div>
+        </div>
 
-        <p><a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Do Incentivo a Projetos Culturais</a></p>
-        <p>Art. 18.  Com o objetivo de incentivar as atividades culturais, a União facultará às pessoas físicas ou jurídicas a opção pela aplicação de parcelas do Imposto sobre a Renda, a título de doações ou patrocínios, tanto no apoio direto a projetos culturais apresentados por pessoas físicas ou por pessoas jurídicas de natureza cultural, como através de contribuições ao FNC, nos termos do art. 5o, inciso II, desta Lei, desde que os projetos atendam aos critérios estabelecidos no art. 1o desta Lei.</p>
-        <p>§ 1o  Os contribuintes poderão deduzir do imposto de renda devido as quantias efetivamente despendidas nos projetos elencados no § 3o, previamente aprovados pelo Ministério da Cultura, nos limites e nas condições estabelecidos na legislação do imposto de renda vigente, na forma de: </p>
-        <p>a) doações; e</p>
-        <p>b) patrocínios.</p>
-        <p>§ 2o  As pessoas jurídicas tributadas com base no lucro real não poderão deduzir o valor da doação ou do patrocínio referido no parágrafo anterior como despesa operacional.</p>
-        <p>§ 3o  As doações e os patrocínios na produção cultural, a que se refere o § 1o, atenderão exclusivamente aos seguintes segmentos:</p>
-        <p>a) artes cênicas;</p>
-        <p>b) livros de valor artístico, literário ou humanístico;</p>
-        <p>c) música erudita ou instrumental; </p>
-        <p>d) exposições de artes visuais;</p>
-        <p>e) doações de acervos para bibliotecas públicas, museus, arquivos públicos e cinematecas, bem como treinamento de pessoal e aquisição de equipamentos para a manutenção desses acervos;</p>
-        <p>f) produção de obras cinematográficas e videofonográficas de curta e média metragem e preservação e difusão do acervo audiovisual; </p>
-        <p>g) preservação do patrimônio cultural material e imaterial.</p>
-        <p>h) construção e manutenção de salas de cinema e teatro, que poderão funcionar também como centros culturais comunitários, em Municípios com menos de 100.000 (cem mil) habitantes.</p>
-      </div>
-    </div>
-  </section>
+        <div class="col-text">
+          <h5>Está na lei:</h5>
 
-  <section class="image-and-text" id="fnc">
-    <div class="container image-left">
-      <div class="col-image pt-0 pb-0">
-        <div class="box">
-          <h3>FNC</h3>
-          <p>O Fundo Nacional da Cultura representa o investimento direto do Estado no fomento à Cultura, já que o apoio a projetos decorre de recursos do orçamento da União. O apoio via FNC promove, por exemplo, a distribuição regional dos recursos de forma equilibrada. O fomento ocorre, principalmente, por meio de editais de seleção – o que proporciona aderência às políticas da Secretaria Especial da Cultura do Ministério da Cidadania, promovendo um equilíbrio regional, social e econômico.</p>
+          <p><a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Do Fundo Nacional da Cultura (FNC)</a></p>
+          <p>Art. 4° Fica ratificado o Fundo de Promoção Cultural, criado pela Lei n° 7.505, de 2 de julho de 1986, que passará a denominar-se Fundo Nacional da Cultura (FNC), com o objetivo de captar e destinar recursos para projetos culturais compatíveis com as finalidades do Pronac e de:</p>
+          <p>I - estimular a distribuição regional eqüitativa dos recursos a serem aplicados na execução de projetos culturais e artísticos;</p>
+          <p>II - favorecer a visão interestadual, estimulando projetos que explorem propostas culturais conjuntas, de enfoque regional;</p>
+          <p>III - apoiar projetos dotados de conteúdo cultural que enfatizem o aperfeiçoamento profissional e artístico dos recursos humanos na área da cultura, a criatividade e a diversidade cultural brasileira;</p>
+          <p>IV - contribuir para a preservação e proteção do patrimônio cultural e histórico brasileiro;</p>
+          <p>V - favorecer projetos que atendam às necessidades da produção cultural e aos interesses da coletividade, aí considerados os níveis qualitativos e quantitativos de atendimentos às demandas culturais existentes, o caráter multiplicador dos projetos através de seus aspectos sócio-culturais e a priorização de projetos em áreas artísticas e culturais com menos possibilidade de desenvolvimento com recursos próprios.</p>
         </div>
       </div>
+    </section>
 
-      <div class="col-text">
-        <h3>Está na lei:</h3>
+    <section class="image-and-text bg-grey-2" id="ficart">
+      <div class="container image-left">
+        <div class="col-image pt-0 pb-0">
+          <div class="box" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/05.jpg');">
+            <h3>Ficart</h3>
+            <p>Os Fundos de Investimento Cultural e Artístico (Ficart) são fundos de captação no mercado, idealizados para apoiar projetos culturais de alta viabilidade econômica e reputacional. No mecanismo, o financiamento do projeto cultural prevê lucro para o investidor. O Ficart, apesar de previsto na legislação que instituiu o Pronac, não está implementado.</p>
+          </div>
+        </div>
 
-        <p><a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Do Fundo Nacional da Cultura (FNC)</a></p>
-        <p>Art. 4° Fica ratificado o Fundo de Promoção Cultural, criado pela Lei n° 7.505, de 2 de julho de 1986, que passará a denominar-se Fundo Nacional da Cultura (FNC), com o objetivo de captar e destinar recursos para projetos culturais compatíveis com as finalidades do Pronac e de:</p>
-        <p>I - estimular a distribuição regional eqüitativa dos recursos a serem aplicados na execução de projetos culturais e artísticos;</p>
-        <p>II - favorecer a visão interestadual, estimulando projetos que explorem propostas culturais conjuntas, de enfoque regional;</p>
-        <p>III - apoiar projetos dotados de conteúdo cultural que enfatizem o aperfeiçoamento profissional e artístico dos recursos humanos na área da cultura, a criatividade e a diversidade cultural brasileira;</p>
-        <p>IV - contribuir para a preservação e proteção do patrimônio cultural e histórico brasileiro;</p>
-        <p>V - favorecer projetos que atendam às necessidades da produção cultural e aos interesses da coletividade, aí considerados os níveis qualitativos e quantitativos de atendimentos às demandas culturais existentes, o caráter multiplicador dos projetos através de seus aspectos sócio-culturais e a priorização de projetos em áreas artísticas e culturais com menos possibilidade de desenvolvimento com recursos próprios.</p>
-      </div>
-    </div>
-  </section>
-
-  <section class="image-and-text bg-grey-2" id="ficart">
-    <div class="container image-left">
-      <div class="col-image pt-0 pb-0">
-        <div class="box">
-          <h3>Ficart</h3>
-          <p>Os Fundos de Investimento Cultural e Artístico (Ficart) são fundos de captação no mercado, idealizados para apoiar projetos culturais de alta viabilidade econômica e reputacional. No mecanismo, o financiamento do projeto cultural prevê lucro para o investidor. O Ficart, apesar de previsto na legislação que instituiu o Pronac, não está implementado.</p>
+        <div class="col-text">
+          <h5>Está na lei:</h5>
+          <p><a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Dos Fundos de Investimento Cultural e Artístico (Ficart)</a></p>
+          <p>Art. 8° Fica autorizada a constituição de Fundos de Investimento Cultural e Artístico (Ficart), sob a forma de condomínio, sem personalidade jurídica, caracterizando comunhão de recursos destinados à aplicação em projetos culturais e artísticos.</p>
+          <p>Art. 9o  São considerados projetos culturais e artísticos, para fins de aplicação de recursos do FICART, além de outros que venham a ser declarados pelo Ministério da Cultura:</p>
+          <p>I - a produção comercial de instrumentos musicais, bem como de discos, fitas, vídeos, filmes e outras formas de reprodução fonovideográficas;</p>
+          <p>II - a produção comercial de espetáculos teatrais, de dança, música, canto, circo e demais atividades congêneres;</p>
+          <p>III - a edição comercial de obras relativas às ciências, às letras e às artes, bem como de obras de referência e outras de cunho cultural;</p>
+          <p>IV - construção, restauração, reparação ou equipamento de salas e outros ambientes destinados a atividades com objetivos culturais, de propriedade de entidades com fins lucrativos;</p>
+          <p>V - outras atividades comerciais ou industrias, de interesse cultural, assim considerados pela SEC/PR, ouvida a CNIC.</p>
+          <p>VI - outras atividades comerciais ou industriais, de interesse cultural, assim consideradas pelo Ministério da Cultura.  </p>
         </div>
       </div>
+    </section>
 
-      <div class="col-text">
-        <h3>Está na lei:</h3>
-        <p><a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Dos Fundos de Investimento Cultural e Artístico (Ficart)</a></p>
-        <p>Art. 8° Fica autorizada a constituição de Fundos de Investimento Cultural e Artístico (Ficart), sob a forma de condomínio, sem personalidade jurídica, caracterizando comunhão de recursos destinados à aplicação em projetos culturais e artísticos.</p>
-        <p>Art. 9o  São considerados projetos culturais e artísticos, para fins de aplicação de recursos do FICART, além de outros que venham a ser declarados pelo Ministério da Cultura:</p>
-        <p>I - a produção comercial de instrumentos musicais, bem como de discos, fitas, vídeos, filmes e outras formas de reprodução fonovideográficas;</p>
-        <p>II - a produção comercial de espetáculos teatrais, de dança, música, canto, circo e demais atividades congêneres;</p>
-        <p>III - a edição comercial de obras relativas às ciências, às letras e às artes, bem como de obras de referência e outras de cunho cultural;</p>
-        <p>IV - construção, restauração, reparação ou equipamento de salas e outros ambientes destinados a atividades com objetivos culturais, de propriedade de entidades com fins lucrativos;</p>
-        <p>V - outras atividades comerciais ou industrias, de interesse cultural, assim considerados pela SEC/PR, ouvida a CNIC.</p>
-        <p>VI - outras atividades comerciais ou industriais, de interesse cultural, assim consideradas pelo Ministério da Cultura.  </p>
-      </div>
-    </div>
-  </section>
-
-  
+  </div>
 
 </main>
 
