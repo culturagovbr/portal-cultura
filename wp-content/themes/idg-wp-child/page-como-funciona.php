81c8b9fbe07df12db43e4e9f8bb003b9b9861228
Daniel Moura <daniel.mouraa@gmail.com>
2019-03-11 15:27:52 -0300

    [UPDATE] Adicionando tela de 'como se inscrever'

---
+++ wp-content/themes/idg-wp-child/page-como-funciona.php
@@ -0,0 +1,179 @@
+<?php /* Template Name: Como Funciona */ ?>
+
+<?php get_header(); ?>
+
+<main id="como-funciona" class="site-main">
+  <div class="container">
+    <div class="row">
+      <?php the_breadcrumb(); ?>
+    </div>
+
+    <?php wp_reset_postdata(); ?>
+
+  </div>
+
+
+  <section class="image-and-text">
+
+    <div class="container image-right no-border">
+      <div class="col-image text-center">
+        <div class="box">
+          <div class="img-wrapper mb-3 mt-0" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/como-funciona-1.png'); border: 1px solid #CCC;">
+          </div>
+        </div>
+      </div>
+
+      <div class="col-text">
+        <h2 class="section-title mb-5">Como Funciona?</h2>
+        <p>Um produtor cultural, artista ou instituição, como um museu ou teatro, por exemplo, planeja fazer um evento cultural – um festival, uma exposição, uma feira de livros, entre outros. Se o próprio produtor não tiver os recursos necessários para fazer esse evento, ele pode recorrer à Secretaria Especial da Cultura do Ministério da Cidadania, por meio da Lei de Incentivo à Cultura, para buscar apoio na realização desse projeto. A proposta apresentada pode, então, ser aprovada para captar o dinheiro junto à iniciativa privada. Ainda que a proposta tenha sido aprovada, isso não significa que a pessoa ou instituição que a propôs, ou seja, o proponente, vá receber o valor necessário para sua execução. É importante ressaltar que a Secretaria Especial da Cultura não distribui verba para a realização dos projetos. Ou seja, os projetos incentivados não recebem recursos originários do orçamento do Ministério da Cidadania.</p>
+
+        <ul class="horizontal-links">
+          <li><a href="#incentivo">Tutoriais</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <section id="info-graphic">
+    <!-- <div class="container"> -->
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/info-caminho-rouanet-2.png" />
+    <!-- </div> -->
+
+    <div class="container">
+      <div class="text pb-5">
+        <hr/>
+        <h2 class="section-title mb-3">Passo a Passo</h2>
+        <p>1) Apresentação de proposta: O proponente (responsável pelo projeto) insere uma proposta cultural no Sistema de Apoio às Leis de Incentivo à Cultura (Salic), de forma eletrônica.</p>
+
+        <p> 2) Análise de admissibilidade: O Ministério da Cidadania realiza a análise de admissibilidade da proposta a partir de critérios objetivos estabelecidos pela Lei 8.313/91 e pela Instrução Normativa em vigor. Se admitida, a proposta se transforma em projeto e recebe autorização para captação de recursos incentivados, a partir de publicação no Diário Oficial da União (DOU). </p>
+
+        <p>3) Análise técnica: o projeto é encaminhado à análise técnica por parecerista da área cultural do projeto. </p>
+
+        <p>4) Análise pela CNIC: Após emissão do parecer técnico, o projeto cultural é apreciado pela Comissão Nacional de Incentivo à Cultura (CNIC), que homologa a execução do projeto.</p>
+
+        <p> 5) Decisão final: o ministro de Estado da Cidadania, em última instância, decide quanto à aprovação ou rejeição do projeto cultural. Historicamente, por convenção, o Ministério acompanha a decisão do órgão consultivo. </p>
+
+        <p>6) Captação: após a aprovação do projeto, cabe ao proponente encontrar as empresas para patrocinar sua ideia. Quando conseguir captar 20% do valor total aprovado, pode iniciar a execução da proposta conforme detalhado no projeto.</p>
+
+        <p>7) Execução: é a realização do projeto, quando o proponente entra em contato com fornecedores, artistas e outros prestadores de serviço que irão ajudar a executar a proposta. Também envolve a realização em si do espetáculo, show, festival, montagem e visitação de exposições, impressão de livros, etc.</p>
+
+        <p>8) Prestação de contas: após o término do projeto, o proponente precisa prestar contas de tudo o que foi realizado: como os recursos foram aplicados, como os objetivos e resultados dos projetos foram alcançados, quantas pessoas foram atingidas pela proposta – tudo acompanhado por notas fiscais, comprovantes de transferência, panfletos, anúncios, matérias de jornal, etc.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="image-and-text bg-grey-2" id="ficart">
+    <div class="container image-right align-center">
+      <div class="col-image pt-0 pb-0">
+        <div class="box">
+          <div class="img-wrapper square mb-3 mt-0" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/05.jpg');"></div>
+        </div>
+      </div>
+
+      <div class="col-text">
+        <h3>Cenic</h3>
+        <p>A Comissão Nacional de Incentivo à Cultura (CNIC) é um colegiado de assessoramento de caráter paritário, formado por representantes dos setores artísticos, culturais e empresariais, em paridade da sociedade civil e do poder público. Ela analisa os projetos culturais, inclusive sob seus aspectos orçamentários, podendo para tanto solicitar informações adicionais, diligenciando o proponente, emitindo parecer conclusivo pela aprovação, total ou parcial, ou rejeição do projeto cultural.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="image-and-text" id="ficart">
+    <div class="container image-left align-center">
+      <div class="col-text">
+        <h3>Como inscrever um projeto?</h3>
+        <p class="mb-5">Para conseguir o apoio da Lei de Incentivo à Cultura, é preciso acessar o Sistema de Apoio às Leis de Incentivo à Cultura (Salic) e cadastrar ali a sua proposta. Para tanto, é preciso descrever detalhadamente a ideia, relatando as razões e os objetivos do projeto, especificando: como e em quanto tempo será executado, como será divulgado, quais profissionais e empresas estarão envolvidos nesse processo e quanto custará a execução dessa ideia, detalhando item a item do orçamento. Confira os tutoriais com o passo a passo da inscrição, nos links abaixo: </p>
+
+          <div class="row mb-4">
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Novo Cadastro e Primeiros Passos da Nova Proposta</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Corpo da Proposta</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Local de Realização e Plano de Distribuição</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Planilha Orçamentária</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Envio da Proposta</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Minhas Solicitações</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Relatório de Comprovação do Objeto</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <div class="feature-card text-center card-1">
+                <a href="http://hmg.cultura.gov.br/acesso-a-informacao/acoes-e-programas/">
+                  <div class="align">
+                    <div class="icon icon-acoes-programadas"></div>
+                    <h3 class="card-title">Comprovação Financeira</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<?php get_footer(); ?>
+
+
+
