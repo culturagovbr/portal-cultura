f0da23130c62bb8bd8b423c44499b6e2d3f0325e
Ricardo Carvalho <rickmanu@gmail.com>
2019-03-13 10:41:12 -0300

    [ADD] Bloco de noticias em destaque

--- wp-content/themes/idg-wp-child/home.php
+++ wp-content/themes/idg-wp-child/home.php
@@ -28,6 +28,127 @@ get_header();
 			} ?>
 		</section>
 
+		<section id="news" class="pb-5 pt-5 bg-grey-2">
+			<div class="container">
+				<div class="row">
+					<div class="overflow-wrapper">
+
+						<?php
+						$args = array(
+							'posts_per_page' => 1,
+							'category_name'  => 'destaquinho-1'
+						);
+						$news_query = new WP_Query( $args ); ?>
+
+						<?php if ( $news_query->have_posts() ) : ?>
+
+							<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+
+							
+								<div class="col-lg-4 mb-5">
+									<?php
+									if ( has_post_thumbnail() ) {
+										$post_thumb = get_the_post_thumbnail_url(get_the_ID(), 'highlight-box');
+									} else {
+										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+									}
+									?>
+									<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
+										<div class="box-body">
+											<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
+												<span class="cat"><?php echo $post_subtitle?></span>
+											<?php endif; ?>
+											<h3 class="box-title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
+									</div>
+								</div>
+							<?php endwhile; ?>
+
+							<?php wp_reset_postdata(); ?>
+
+						<?php endif; ?>
+
+						<?php
+						$args = array(
+							'posts_per_page' => 1,
+							'category_name'  => 'destaquinho-2'
+						);
+						$news_query = new WP_Query( $args ); ?>
+
+						<?php if ( $news_query->have_posts() ) : ?>
+
+							<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+								<div class="col-lg-4 mb-5">
+									<?php
+									if ( has_post_thumbnail() ) {
+										$post_thumb = get_the_post_thumbnail_url(get_the_ID(), 'highlight-box');
+									} else {
+										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+									}
+									?>
+									<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
+										<div class="box-body">
+											<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
+												<span class="cat"><?php echo $post_subtitle?></span>
+											<?php endif; ?>
+											<h3 class="box-title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
+									</div>
+								</div>
+							<?php endwhile; ?>
+
+							<?php wp_reset_postdata(); ?>
+
+						<?php endif; ?>
+
+						<?php
+						$args = array(
+							'posts_per_page' => 1,
+							'category_name'  => 'destaquinho-3'
+						);
+						$news_query = new WP_Query( $args ); ?>
+
+						<?php if ( $news_query->have_posts() ) : ?>
+
+							<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+								<div class="col-lg-4 mb-5">
+									<?php
+									if ( has_post_thumbnail() ) {
+										$post_thumb = get_the_post_thumbnail_url(get_the_ID(), 'highlight-box');
+									} else {
+										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+									}
+									?>
+									<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
+										<div class="box-body">
+											<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
+												<span class="cat"><?php echo $post_subtitle?></span>
+											<?php endif; ?>
+											<h3 class="box-title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
+									</div>
+								</div>
+							<?php endwhile; ?>
+
+							<?php wp_reset_postdata(); ?>
+
+						<?php endif; ?>
+					</div>
+
+					<div class="col-lg-12 text-center">
+						<a href="<?php echo home_url( '/categoria/noticias/' ); ?>" class="btn text-uppercase mt-1">Mais
+							notícias</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
 		<?php if ( !idg_wp_get_option('_home_widgets_sections_disable') ):  ?>
 
 			<?php
