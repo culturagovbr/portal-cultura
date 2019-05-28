1281ed7f38d7096f5b1b43be26b19ee1d361f65d
Ricardo Carvalho <rickmanu@gmail.com>
2019-05-28 10:59:29 -0300

    [UPDATE] IDG - Oscar pagina inicial

--- wp-content/themes/idg-wp-oscar/home.php
+++ wp-content/themes/idg-wp-oscar/home.php
@@ -146,9 +146,132 @@ get_header();
 					</div>
 
 					<div class="col-lg-12 text-center">
-						<a href="<?php echo home_url('/category/noticias/'); ?>" class="btn text-uppercase mt-1">Mais
-							notícias</a>
+						<a href="<?php echo home_url('/category/noticias/'); ?>" class="btn text-uppercase mt-1">Mais notícias</a>
 					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="multimidia" class="mt-5">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-lg-12">
+						<h2 class="section-title mb-5 text-center">Filmes indicados</h2>
+					</div>
+
+					<?php
+					$args      = array(
+						'post_type'  => 'multimedia',
+						'posts_per_page' => 1,
+					);
+					$multimedia_query = new WP_Query( $args ); ?>
+
+					<?php if ( $multimedia_query->have_posts() ) : ?>
+
+						<?php while ( $multimedia_query->have_posts() ) : $multimedia_query->the_post(); ?>
+							<?php
+							$taxonomy_names = wp_get_post_terms(get_the_ID(), 'multimedia-type');
+
+							if ( has_post_thumbnail() ) {
+								$multimedia_thumb = get_the_post_thumbnail_url( get_the_ID(), 'multimedia-feature' );
+							} else {
+
+								if( $taxonomy_names[0]->slug == 'video' ):
+									$video_id = embeded_youtube_video_id( get_the_content() );
+
+									if( $video_id ){
+										$multimedia_thumb = 'https://img.youtube.com/vi/'. $video_id .'/maxresdefault.jpg';
+									} else {
+										$multimedia_thumb = get_template_directory_uri() . '/assets/img/media-'. $taxonomy_names[0]->slug .'-thumb.png';
+									}
+
+								else:
+									$multimedia_thumb = get_template_directory_uri() . '/assets/img/media-'. $taxonomy_names[0]->slug .'-thumb.png';
+								endif;
+							}
+							?>
+							<div class="highlight <?php echo $taxonomy_names[0]->slug ?>" style="background-image: url('<?php echo $multimedia_thumb; ?>');">
+								<a href="<?php the_permalink(); ?>">
+									<div class="align">
+										<div class="text">
+											<h3><?php the_title(); ?></h3>
+											<span><?php echo idg_excerpt(30); ?></span>
+											<span><u>Ver Mais</u></span>
+										</div>
+									</div>
+								</a>
+								<?php if( $taxonomy_names[0]->slug == 'video' ) :
+									if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', get_the_content(), $match)) {
+										echo '<div id="play-multimedia-video" data-video-src="'. $match[1] .'"><i class="icon icon-play_btn"></i></div>';
+									}
+								endif; ?>
+							</div>
+						<?php endwhile; ?>
+
+						<?php wp_reset_postdata(); ?>
+
+					<?php endif; ?>
+
+					<div class="container">
+						<div class="row">
+
+							<?php
+							$args      = array(
+								'post_type'  => 'multimedia',
+								'posts_per_page' => 3,
+								'offset' => 1,
+							);
+							$multimedia_query = new WP_Query( $args ); ?>
+
+							<?php if ( $multimedia_query->have_posts() ) : ?>
+
+								<?php while ( $multimedia_query->have_posts() ) : $multimedia_query->the_post(); ?>
+									<?php
+									$taxonomy_names = wp_get_post_terms(get_the_ID(), 'multimedia-type');
+
+									if ( has_post_thumbnail() ) {
+										$multimedia_thumb = get_the_post_thumbnail_url( get_the_ID(), 'multimedia-feature' );
+									} else {
+
+										if( $taxonomy_names[0]->slug == 'video' ):
+											$video_id = embeded_youtube_video_id( get_the_content() );
+
+											if( $video_id ){
+												$multimedia_thumb = 'https://img.youtube.com/vi/'. $video_id .'/maxresdefault.jpg';
+											} else {
+												$multimedia_thumb = get_template_directory_uri() . '/assets/img/media-'. $taxonomy_names[0]->slug .'-thumb.png';
+											}
+
+										else:
+											$multimedia_thumb = get_template_directory_uri() . '/assets/img/media-'. $taxonomy_names[0]->slug .'-thumb.png';
+										endif;
+									}
+									?>
+
+									<div class="col">
+										<a href="<?php the_permalink(); ?>">
+											<div class="highlight-box" style="background-image: url('<?php echo $multimedia_thumb; ?>')">
+												<div class="box-body">
+													<h3 class="box-title"><?php the_title(); ?></h3>
+												</div>
+											</div>
+										</a>
+									</div>
+
+								<?php endwhile; ?>
+
+								<?php wp_reset_postdata(); ?>
+
+							<?php endif; ?>
+						</div>
+
+					</div>
+
+					<div class="multimidia-footer col-lg-12 text-center">
+						<a href="<?php echo get_bloginfo('url'); ?>/multimedia" class="btn text-uppercase mt-5 btn-ver-mais">Mais filmes</a>
+					</div>
+
 				</div>
 			</div>
 		</section>
