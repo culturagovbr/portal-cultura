3af11d4854e627f6c16548123bc42fa7fc6362b5
Ricardo Carvalho <rickmanu@gmail.com>
2019-05-31 14:19:42 -0300

    [UPDATE] Ajustes no submodulo para corrigir conflito no admin

--- wp-content/plugins/oscar-minc/assets/oscar-minc-admin.js
+++ wp-content/plugins/oscar-minc/assets/oscar-minc-admin.js
@@ -7,6 +7,9 @@
         init: function() {
             // console.log('Admin scripts here!');
             $('#oscar_minc_deadline_time').mask('00/00/0000 00:00');
+            $('#oscar_minc_schedule_time_1').mask('00/00/0000 00:00');
+            $('#oscar_minc_schedule_time_2').mask('00/00/0000 00:00');
+            $('#oscar_minc_schedule_time_3').mask('00/00/0000 00:00');
         }
     };
 })(jQuery);
