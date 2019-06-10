c671fb5522f7b6e8f1601f7241ea5b454130edef
Daniel Moura <daniel.mouraa@gmail.com>
2019-06-10 15:20:42 -0300

    [UPDATE] Ajustando data de encerramento das inscricoes

---
+++ wp-content/themes/idg-wp-oscar/page-template-filmes-indicados.php
@@ -0,0 +1,105 @@
+<?php /* Template Name: CNIC */ ?>
+
+<?php get_header(); ?>
+
+<main id="page-cnic" class="site-main">
+  <div class="container">
+    <div class="row">
+      <?php the_breadcrumb(); ?>
+    </div>
+
+    <?php wp_reset_postdata(); ?>
+
+  </div>
+
+<!-- <section class="image-and-text">
+  <div class="container image-right no-border">
+    <div class="col-text">
+      <h2 class="section-title">Filmes Indicados</h2>
+    </div>
+  </div>
+</section> -->
+
+<section class="timeline">
+  <ul>
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-17.png');"></figure>
+
+        <time>1945</time> Quando a composição “Rio de Janeiro”, do brasileiro <a href="https://pt.wikipedia.org/wiki/Ary_Barroso" target="_blank">Ary Barroso</a>, disputou a estatueta de <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_can%C3%A7%C3%A3o_original" target="_blank">Melhor Canção Original</a> pelo filme norte-americano Brazil.
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-19.png');"></figure>
+
+        <time>1960</time> O drama ítalo-franco-brasileiro Orfeu do Carnaval ou Orfeu Negro (na <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_can%C3%A7%C3%A3o_original" target="_blank">França</a>, Orphée Noir; na <a href="https://pt.wikipedia.org/wiki/It%C3%A1lia" target="_blank">Itália</a>, Orfeo Negro) venceu o <a href="https://pt.wikipedia.org/wiki/Oscar_de_Melhor_Filme_Estrangeiro" target="_blank">Oscar de Melhor Filme Estrangeiro</a>, mas representando a França, não o Brasil.
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-20.png');"></figure>
+
+        <time>1963</time> O país teve seu próprio representante entre os melhores filmes estrangeiros do ano com <a href="https://pt.wikipedia.org/wiki/O_Pagador_de_Promessas" target="_blank">O Pagador de Promessas</a>. 
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-22.png');"></figure>
+
+        <time>1979</time> A coprodução brasileira <a href="https://pt.wikipedia.org/wiki/Raoni_(filme)" target="_blank">Raoni</a>, que mostra a vida do líder indígena brasileiro <a href="https://pt.wikipedia.org/wiki/Raoni_Metuktire" target="_blank">Raoni Metuktire</a>, disputou a categoria de <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_document%C3%A1rio_de_longa-metragem" target="_blank">Melhor Documentário</a>.
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-23.png');"></figure>
+
+        <time>1982</time> <a href="https://pt.wikipedia.org/wiki/Tet%C3%AA_Vasconcellos" target="_blank">Tetê Vasconcellos</a> concorreu na categoria Documentário por seu trabalho em <a href="https://pt.wikipedia.org/wiki/El_Salvador:_Another_Vietnam" target="_blank">El Salvador: Another Vietnam</a>. 
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-24.png');"></figure>
+
+        <time>1986</time> A coprodução brasileira <a href="https://pt.wikipedia.org/wiki/O_Beijo_da_Mulher-Aranha" target="_blank">O Beijo da Mulher-Aranha</a> recebeu quatro indicações ao Oscar, incluindo <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_filme" target="_blank">Melhor Filme</a> e <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_diretor" target="_blank">Melhor Diretor</a> para o brasileiro naturalizado Hector Babenco.
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-17.png');"></figure>
+
+        <time>1945</time> Quando a composição “Rio de Janeiro”, do brasileiro <a href="https://pt.wikipedia.org/wiki/Ary_Barroso" target="_blank">Ary Barroso</a>, disputou a estatueta de <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_can%C3%A7%C3%A3o_original" target="_blank">Melhor Canção Original</a> pelo filme norte-americano Brazil.
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-17.png');"></figure>
+
+        <time>1945</time> Quando a composição “Rio de Janeiro”, do brasileiro <a href="https://pt.wikipedia.org/wiki/Ary_Barroso" target="_blank">Ary Barroso</a>, disputou a estatueta de <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_can%C3%A7%C3%A3o_original" target="_blank">Melhor Canção Original</a> pelo filme norte-americano Brazil.
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <figure style="background-image: url('http://oscar.cultura.gov.br/wp-content/uploads/2018/06/image-17.png');"></figure>
+
+        <time>1945</time> Quando a composição “Rio de Janeiro”, do brasileiro <a href="https://pt.wikipedia.org/wiki/Ary_Barroso" target="_blank">Ary Barroso</a>, disputou a estatueta de <a href="https://pt.wikipedia.org/wiki/Oscar_de_melhor_can%C3%A7%C3%A3o_original" target="_blank">Melhor Canção Original</a> pelo filme norte-americano Brazil.
+      </div>
+    </li>
+
+  </ul>
+</section>
+
+</main>
+
+<?php get_footer(); ?>
+
+
+
