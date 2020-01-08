338580721eb4874509fc77b43fc5feb8338d798d
Walquirio Saraiva Rocha <walquiriosaraiva@gmail.com>
2020-01-08 18:28:49 -0300

    [update] alterando texto conforme solicitado pela equipe gestora ASCOM

--- wp-content/themes/idg-wp-child/page-template-cnic.php
+++ wp-content/themes/idg-wp-child/page-template-cnic.php
@@ -31,13 +31,11 @@
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/cnic.jpg?2" />
           </div>
 
-          <p>A Comissão Nacional de Incentivo à Cultura é um colegiado formado por 21 membros da sociedade civil, sendo sete titulares e 14 suplentes. Também integram a Comissão o secretário especial da Cultura, que a preside, os presidentes das sete entidades culturais vinculadas ao Ministério da Cidadania (Agência Nacional do Cinema – Ancine, Instituto do Patrimônio Histórico e Artístico Nacional – Iphan, Instituto Brasileiro de Museus – Ibram, Fundação Biblioteca Nacional, Fundação Casa de Rui Barbosa, Fundação Nacional de Artes – Funarte e Fundação Cultural Palmares) e o presidente da entidade nacional que congrega os Secretários de Cultura das unidades federativas. No total, são 16 membros titulares, com dois suplentes cada.</p>
+          <p>A Comissão Nacional de Incentivo à Cultura é um colegiado formado por 21 membros da sociedade civil, sendo sete titulares e 14 suplentes. Também integram a Comissão integrantes do Poder Público, o Ministro de Estado da Cidadania, que a preside, os presidentes das sete entidades culturais vinculadas ao Ministério da Cidadania (Agência Nacional do Cinema – Ancine, Instituto do Patrimônio Histórico e Artístico Nacional – Iphan, Instituto Brasileiro de Museus – Ibram, Fundação Biblioteca Nacional - FBN, Fundação Casa de Rui Barbosa - FCRB, Fundação Nacional de Artes – Funarte e Fundação Cultural Palmares - FCP) e o presidente da entidade nacional que congrega os Secretários de Cultura das unidades federativas.</p>
 
-          <p>A principal função da CNIC é a de subsidiar as decisões sobre a aprovação de projetos culturais que pleiteiam incentivo fiscal por meio da <a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Lei de Incentivo à Cultura</a>.</p>
+          <p>A principal função da CNIC é apreciar os projetos culturais que pleiteiam incentivo fiscal por meio da <a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Lei de Incentivo à Cultura</a> e subsidiar nas decisões do Ministro de Estado da Cidadania.</p>
 
           <p>Com o objetivo de garantir diversidade à Comissão, as indicações dos representantes da sociedade civil tem, obrigatoriamente, que considerar as cinco regiões brasileiras e contemplar as seguintes áreas: artes cênicas, audiovisual, música, artes visuais, arte digital e eletrônica, patrimônio cultural, inclusive museológico e expressões da cultura negra, indígena e das populações tradicionais; humanidades, inclusive literatura e obras de referência.</p>
-
-          <p>Conforme o art. 39 do <a href="http://www.planalto.gov.br/ccivil_03/_Ato2004-2006/2006/Decreto/D5761.htm" target="_blank">Decreto 5.761/2006</a>, o mandato dos membros da CNIC é de dois anos, podendo ser renovado uma vez. Também fazem parte da Comissão o Secretário Especial da Cultura, que a preside, os presidentes de cada uma das instituições vinculadas ao Ministério da Cidadania (Agência Nacional do Cinema – Ancine, Instituto do Patrimônio Histórico e Artístico Nacional – Iphan, Instituto Brasileiro de Museus – Ibram, Fundação Biblioteca Nacional, Fundação Casa de Rui Barbosa, Fundação Nacional de Artes – Funarte e Fundação Cultural Palmares) e o presidente da entidade nacional que congrega os Secretários de Cultura das unidades federativas.</p>
 
           <p>O trabalho da comissão é voluntário, ou seja, não é remunerado. Os membros da CNIC recebem apenas ajuda de custo para participar das reuniões presenciais. A escolha dos representantes da sociedade civil é feita pelo ministro da Cidadania, com base em uma lista de nomes indicados por entidades habilitadas.</p>
       </div>
@@ -49,7 +47,7 @@
       <div class="col-text">
         <h2 class="section-title">O que faz a Comissão?</h2>
 
-        <p>As reuniões da CNIC ocorrem mensalmente. Cabe à comissão dar parecer favorável ou não aos projetos apresentados à Secretaria Especial da Cultura como candidatos ao incentivo fiscal oferecido pela <a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Lei de Incentivo à Cultura</a>. As decisões precisam ser referendadas pelo Secretário Especial da Cultura, que preside a Comissão.</p>
+        <p>As reuniões da CNIC ocorrem mensalmente. Cabe à comissão dar parecer favorável ou não aos projetos apresentados à Secretaria Especial da Cultura como candidatos ao incentivo fiscal oferecido pela <a href="http://www.planalto.gov.br/ccivil_03/leis/L8313cons.htm" target="_blank">Lei de Incentivo à Cultura</a>. As decisões precisam ser referendadas pelo Ministro de Estado da Cidadania, que preside a Comissão ou pelo seu suplente, o Secretário de Fomento e Incentivo à Cultura.</p>
 
         <p>Com o projeto aprovado pela Secretaria Especial da Cultura o proponente está autorizado a procurar junto a empresas ou pessoas físicas o apoio ou patrocínio à iniciativa, que poderá ser posteriormente deduzido pelo apoiador, totalmente ou em parte, do imposto de renda devido.</p>
       </div>
@@ -61,7 +59,7 @@
       <div class="col-text">
         <h2 class="section-title">Quem é quem</h2>
 
-        <p class="mb-5">Comissão empossada em 18/03/2019, para o biênio 2019/2020:</p>
+        <p class="mb-5">Comissão Nacional de Incentivo à Cultura - CNIC, do biênio 2019/2020:</p>
 
         <div class="row mb-4">
           <div class="col">
@@ -108,8 +106,13 @@
                   <tbody>
                      <tr>
                         <td>
-                           <strong>Titular:</strong> Paula Sulmonetti Tavares de Azevedo – Associação Nacional das Entidades Não Lucrativas
+                           <strong>Titular:</strong> Paula Sulmonetti Tavares de Azevedo – Associação Nacional das Entidades Culturais Não Lucrativas
                         </td>
+                     </tr>
+                     <tr>
+                         <td>
+                             <strong>1º Suplente:</strong> André José Kryszczun – Indicação do Ministro de Estado da Cidadania
+                         </td>
                      </tr>
                      <tr>
                         <td>
@@ -140,6 +143,11 @@
                         </td>
                      </tr>
                      <tr>
+                         <td>
+                             <strong>1ª Suplente:</strong> Flávia Martins Furtado – Indicação do Ministro de Estado da Cidadania
+                         </td>
+                     </tr>
+                     <tr>
                         <td>
                            <strong>2º Suplente:</strong> Cláudio Alano Cohen Bezerra – Indicação do Ministro Audiovisual
                         </td>
@@ -162,12 +170,12 @@
                   <tbody>
                      <tr>
                         <td>
-                           <strong>Titular:</strong> Bruno Luis Margraf Gehring – Associação Brasileira das Desenvolvedoras dos Jogos Eletrônicos 
+                           <strong>Titular:</strong> Thiago Wanderley de Freitas – Associação Brasileira das Desenvolvedoras de Jogos Eletrônicos
                         </td>
                      </tr>
                      <tr>
                         <td>
-                           <strong>1º Suplente:</strong> Thiago Wanderley de Freitas – Associação Brasileira das Desenvolvedoras dos Jogos Eletrônicos
+                           <strong>1º Suplente:</strong> Bruno Luis Margraf Gehring – Associação Brasileira das Desenvolvedoras de Jogos Eletrônicos
                         </td>
                      </tr>
                   </tbody>
@@ -265,7 +273,7 @@
                      </tr>
                      <tr>
                         <td>
-                           <strong>2º Suplente:</strong> Carlos Eugênio Trevi - Confederação Nacional das Indústrias Financeiras
+                           <strong>2º Suplente:</strong> Carlos Eugênio Trevi - Confederação Nacional das Instituições Financeiras
                         </td>
                      </tr>
                   </tbody>
@@ -274,7 +282,7 @@
           </div>
         </div>
 
-        <p class="text-center m-0"><a href="http://leideincentivoacultura.cultura.gov.br/wp-content/uploads/2019/09/Calend%C3%A1rio-atualizado-29.09.pdf" target="_blank" class="btn m-0">Calendário de reuniões 2019</a></p>
+        <p class="text-center m-0"><a href="http://cultura.gov.br/wp-content/uploads/2020/01/CALENDÁRIO-CNIC-Versão-1.pdf" target="_blank" class="btn m-0">Calendário de reuniões 2020</a></p>
       </div>
     </div>
   </section>
